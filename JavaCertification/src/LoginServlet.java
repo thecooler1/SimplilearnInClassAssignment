eec2a2803ee5a1aad018e885164b0608b45901ab
thecooler1 <89164333+thecooler1@users.noreply.github.com>
2021-08-19 10:55:16 -0400

    Version 1.0
    Class Created Default Files.
    Only Register Functionality

---
+++ JavaCertification/src/LoginServlet.java
@@ -0,0 +1,39 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name=request.getParameter("name");
+		String password=request.getParameter("password");
+		PrintWriter pw=response.getWriter();
+		if(name.equals(password)) {
+			//PW
+			response.sendRedirect("success.html");
+		}
+		else {
+			pw.print("login failed");
+		}
+	}
+
+}
